d75a269cdfbcd4583d163ba136c6600b460564d6
AB4722 <ab4722@ic.ac.uk>
2024-01-20 18:22:18 +0000

    Initial Commit

---
+++ Testing2/Testing2App.swift
@@ -0,0 +1,17 @@
+//
+//  Testing2App.swift
+//  Testing2
+//
+//  Created by Angus Bloch on 20/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Testing2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
